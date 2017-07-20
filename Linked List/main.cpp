29e333ce4ccf4a4b4010436a39feb27067ef44b0
Brett Markowitz <brettmarkowitz@MacBook-Air-2.local>
2017-07-19 23:17:32 -0400

    Initial Commit

---
+++ Linked List/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Linked List
+//
+//  Created by Brett Markowitz on 7/19/17.
+//  Copyright © 2017 Brett Markowitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
